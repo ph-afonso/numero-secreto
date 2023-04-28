d75d87a08b6c82adc0d0680070b64f10cc830d51
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2023-04-27 22:01:59 -0300

    Recognition

---
+++ app/validacao.js
@@ -0,0 +1,59 @@
+function verificaChute () {
+
+    const numero = + chute;
+
+    if (chuteInvalido(numero)) {
+
+       elementoChute.innerHTML += '<div>Valor Inválido</div>'
+       return
+        
+    }
+
+    if (numeroForMaiorOuMenorPermitido(numero)) {
+
+        elementoChute.innerHTML += '<div>Valor inválido: o número secreto precisa estar entre' + menorValor + 'e ' + maiorValor + '</div>';
+        return
+
+    }
+
+    if (numero === numeroSecreto) {
+
+
+        documento.body.innerHTML = `
+        
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        
+        `
+
+    } else if (numero > numeroSecreto) {
+
+        elementoChute.innerHTML += `
+        
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+
+        `;
+
+    } else {
+
+        elementoChute.innerHTML += `
+        
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+
+        `;
+
+    }
+
+}
+
+function chuteInvalido(numero) {
+
+    return Number.isNaN(numero);
+
+}
+
+function numeroForMaiorOuMenorPermitido(numero) {
+
+    return numero > maiorValor || numero < menorValor;
+
+}
